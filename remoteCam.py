75d724eceb3b5e54e2d998fb5436d94e556c6036
Morgan Heijdemann <targhan@gmail.com>
2016-06-01 14:43:46 +0800

    init

---
+++ remoteCam.py
@@ -0,0 +1,7 @@
+#bootstrap for project remoteCam on RPi3
+import sys,os
+if __import__("sys").version_info[:2]<(2,7,0):
+  sys.stderr.write("You need python 2.7 or later to run this script, you have "+str(__import__("sys").version_info[:2])+".\n")
+  exit(1)
+
+print("Ready.")
