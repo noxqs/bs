c8680ad5e0b682aa1d871c5f1fce30df72d9a78d
Morgan Heijdemann <targhan@gmail.com>
2016-06-01 15:33:02 +0800

    qwe

--- remoteCam.py
+++ remoteCam.py
@@ -1,3 +1,19 @@
 #bootstrap for project remoteCam on RPi3
+import os,sys
+import subprocess
+
+if not os.geteuid() == 0:
+    sys.exit("\nOnly root can run this script\n")
+
+
+subprocess.call("sudo apt-get install autossh python-picamera pi-bluetooth subversion")
+subprocess.call("sudo apt-get install python-zbar")
+
+#wget http://www.kernel.org/pub/linux/bluetooth/bluez-5.40.tar.xz
+#tar xvf bluez-5.40.tar.xz
+#cd bluez-5.40
+#./configure --disable-systemd
+#make
+
 
 print("Ready.")
